5ec7c8d02ceddd475bb5b0b56b1fd147407f480f
Leo Dion <leogdion@brightdigit.com>
2019-06-24 17:10:21 -0400

    fixed spec for test

--- spec/speculid_action_spec.rb
+++ spec/speculid_action_spec.rb
@@ -1,7 +1,7 @@
 describe Fastlane::Actions::SpeculidAction do
   describe '#run' do
     it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The speculid plugin is working!")
+      expect(Fastlane::UI).to receive(:error).with("Speculid is not installed. Please go to https://speculid.com to download and install.")
 
       Fastlane::Actions::SpeculidAction.run(nil)
     end
